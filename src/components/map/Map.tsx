7010919e56969bff211531346049f9fe30366769
Tim Rayner <tim.rayner2020@gmail.com>
2024-03-14 23:41:14 +0000

    update: updated map polygon styles

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,56 +1,93 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, {
+  useRef,
+  useEffect,
+  useState,
+  useMemo,
+  useCallback,
+} from "react";
 import * as THREE from "three";
 //@ts-ignore
-import countriesJSON from "../map/countries.geojson";
+import { COUNTRIES_DATA } from "../../data/countries_data";
+//@ts-ignore
+import HEX_DATA from "../map/countries.geojson";
 //@ts-ignore
 import Globe from "react-globe.gl";
 
-export default function Home() {
-  const { useEffect, useRef } = React;
-
-  const globeEl = useRef();
-
-  useEffect(() => {
-    const globe = globeEl.current;
-
-    // Auto-rotate
-    //@ts-ignore
-    globe.controls().autoRotate = true;
-    //@ts-ignore
-    globe.controls().autoRotateSpeed = 0.35;
-  }, []);
+export default function Map() {
+  const { useState, useEffect, useMemo } = React;
 
   const [countries, setCountries] = useState({ features: [] });
+  const [labelsData, setLabelsData] = useState([]);
+  const [hoverD, setHoverD] = useState();
+
+  // useEffect(() => {
+  //   fetch(HEX_DATA)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setCountries(data);
+
+  //       if (labelsData.length > 0) return;
+  //       const labels = countryLabelData.map((feat: any) => ({
+  //         lat: feat.latitude,
+  //         lng: feat.latitude,
+  //         label: feat.name,
+  //       }));
+  //       //@ts-ignore
+  //       setLabelsData(labels);
+  //     });
+  // }, []);
 
   useEffect(() => {
-    console.log(countriesJSON);
     // load data
-    fetch(countriesJSON)
+    fetch(HEX_DATA)
       .then((res) => res.json())
       .then(setCountries);
   }, []);
 
+  //@ts-ignore
+  const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
+
+  // GDP per capita (avoiding countries with small pop)
+  const getVal = (feat: any) =>
+    feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+
+  const maxVal = useMemo(
+    () => Math.max(...countries.features.map(getVal)),
+    [countries]
+  );
+
+  colorScale.domain([0, maxVal]);
+
   return (
-    <div className="home">
-      <Globe
-        ref={globeEl}
-        animateIn={true}
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-        hexPolygonsData={countries.features}
-        hexPolygonResolution={3}
-        hexPolygonMargin={0.3}
-        hexPolygonUseDots={true}
-        hexPolygonColor={() =>
-          `#${Math.round(Math.random() * Math.pow(2, 24))
-            .toString(16)
-            .padStart(6, "0")}`
-        }
-        // @ts-ignore
-        hexPolygonLabel={({ properties: d }) => `
-        <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+    <Globe
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      lineHoverPrecision={0}
+      polygonsData={countries.features}
+      labelsData={labelsData}
+      //@ts-ignore
+      labelText={(d) => d.text}
+      labelSize={1.6}
+      labelColor={useCallback(() => "white", [])}
+      labelAltitude={0.15}
+      polygonAltitude={(d) => (d === hoverD ? 0.05 : 0.01)}
+      polygonCapColor={
+        (d) => (d === hoverD ? "steelblue" : "steelblue") //colorScale(getVal(d))
+      }
+      polygonSideColor={() => "rgba(0, 100, 0, 0.35)"}
+      polygonStrokeColor={() => "rgba(0, 100, 0, 0.15)"}
+      //@ts-ignore
+      polygonLabel={({ properties: d }) => `
+        <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+        GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
         Population: <i>${d.POP_EST}</i>
       `}
-      />
-    </div>
+      //@ts-ignore
+      onPolygonHover={setHoverD}
+      polygonsTransitionDuration={300}
+      hexPolygonResolution={3} //values higher than 3 makes it buggy
+      hexPolygonMargin={0.62}
+      hexPolygonColor={useCallback(() => "#1b66b1", [])}
+    />
   );
 }
