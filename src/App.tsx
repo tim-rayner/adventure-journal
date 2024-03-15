d39b04c5dbef70b10bb9e69ab1b5c1c71bf8e920
Tim Rayner <tim.rayner2020@gmail.com>
2024-03-15 15:24:47 +0000

    update: installed PrimeReact lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Home from "./views/Home";
+import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <PrimeReactProvider>
+        <Home />
+      </PrimeReactProvider>
     </div>
   );
 }
